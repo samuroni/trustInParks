81405396aac8ce55a930d853e7541a2ddb4886d3
“samuroni” <samuroni2@gmail.com>
2021-02-20 15:22:48 +0000

    navbar component converted to hooks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import logo from "../images/logo.svg"
 import { FaAlignRight } from "react-icons/fa"
 import {Link} from "react-router-dom"
-import { FaBeer } from 'react-icons/fa';
 
-export default class Navbar extends Component {
+const Navbar = () => {
 
-    state={
-        isOpen: false
-    }
-    handletoggle = () =>{
-        this.setState({isOpen:!this.setState.isOpen})
-    }
+    const [isOpen, setIsOpen] = useState(false)
 
-    render() {
-        return (
+    function handleToggle () {
+        setIsOpen(!isOpen)
+    };
+
+    return (
+        <div>
             <nav className="navbar">
                 <div className="nav-center">
                     <div className="nav-header">
-                        <Link to="/">
-                            <img src={logo} alt="Beach Resort"></img>
-                        </Link>
-                        <button type="button" className="nav-btn" onClick={this.handletoggle}>
-                            <FaAlignRight className="nav-icon" />
+                         <Link to="/">
+                             <img src={logo} alt="Beach Resort"></img>
+                         </Link>
+                        <button type="button" className="nav-btn" onClick={handleToggle}>
+                             <FaAlignRight className="nav-icon" />                         
                         </button>
                     </div>
-                    <ul className={this.state.isOpen?"nav-links show-nav":"nav-links"}>
+                    <ul className={isOpen? "nav-links show-nav" : "nav-links"}>
                         <li>
                             <Link to="/">Home</Link>
                         </li>
@@ -35,6 +33,48 @@ export default class Navbar extends Component {
                     </ul>
                 </div>
             </nav>
-        )
-    }
-}
+        </div>
+    );
+};
+
+export default Navbar;
+
+
+
+
+// export default class Navbar extends Component {
+
+//         isOpen: false
+//     }
+//     handleToggle = () =>{
+//         this.setState({isOpen:!this.state.isOpen})
+//         console.log(this.state.isOpen)
+//     }
+
+//     render() {
+//         return (
+//             <nav className="navbar">
+//                 <div className="nav-center">
+//                     <div className="nav-header">
+//                         <Link to="/">
+//                             <img src={logo} alt="Beach Resort"></img>
+//                         </Link>
+//                         <button type="button" className="nav-btn" onClick={this.handleToggle}>
+//                             <FaAlignRight className="nav-icon" />
+//                         </button>
+//                     </div>
+//                     <ul className={this.state.isOpen? "nav-links show-nav" : "nav-links"}>
+//                         <li>
+//                             <Link to="/">Home</Link>
+//                         </li>
+//                         <li>
+//                             <Link to="/rooms">Rooms</Link>
+//                         </li>
+//                     </ul>
+//                 </div>
+//             </nav>
+//         )
+//     }
+// }
+
+

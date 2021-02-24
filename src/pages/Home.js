a925abf56413f954c098fba84af2410ff3b43792
“samuroni” <samuroni2@gmail.com>
2021-02-24 15:58:25 +0000

    banner created for each page, section services on home page created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
+import Services from '../components/Services'
 
 export default function Home() {
     return (
-        <div>
-            hello from home page
-        </div>
+        <>
+            <Hero> 
+                <Banner title="Luxorious Rooms" subtitle="Deluxe rooms starting at £299">
+                    <Link to="./rooms" className="btn-primary">Our Rooms</Link>
+                </Banner>
+            </Hero>
+            <Services />
+        </>
     )
 }
 
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
+
 
 export default function Rooms() {
     return (
-        <div>
-            Hello from rooms
-        </div>
+        <Hero hero= "roomsHero">
+            <Banner title="Our Rooms">
+                <Link to="./" className="btn-primary">Back to Home</Link>
+            </Banner>
+        </Hero>
     )
 }
+
+

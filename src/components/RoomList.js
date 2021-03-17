7323cdd1558f1726985fd790a4ae8d20235cae6c
“samuroni” <samuroni2@gmail.com>
2021-03-17 17:15:31 +0000

    4.58.00 rooms filter component completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Room from './Room'
 
 export default function RoomList({rooms}) {
-    console.log(rooms)
     if(rooms.length === 0){
         return (
         <div className='empty-search'>
